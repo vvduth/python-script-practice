6b743f02b8419284e9aa00a83cc15ea68038d20d
Duc Thai <ducthai060501@gmail.com>
2024-11-06 14:07:43 +0200

    yaml and json

---
+++ json.py
@@ -0,0 +1,39 @@
+{"Striking" : ["Muay Thai" , "Boxing","Karate" , "Kickboxing"],
+ "Grappling": ["Wrestling","Ju Jitsu", "Sambo"] ,
+ "mma_facts": {"league": "ufc"}
+ }
+
+{
+    "Striking": [
+        "Muay Thai",
+        "Boxing",
+        "Karate",
+        "Kickboxing"
+    ],
+    "Grappling": [
+        "Wrestling",
+        "Ju Jitsu",
+        "Sambo"
+    ],
+    "mma_facts": {
+        "league": "ufc"
+    }
+}
+"""
+    
+---
+Striking:
+- Muay Thai
+- Boxing
+- Karate
+- Kickboxing
+Grappling:
+- Wrestling
+- Ju Jitsu
+- Sambo
+mma_facts:
+  league: ufc
+
+    
+
+"""
